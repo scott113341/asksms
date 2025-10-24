ecca28234d334115d6ae12160bda6aa5ba2a1ffc
Scott Hardy <scott.the.hardy@gmail.com>
2025-10-23 23:50:41 -0600

    Refactor the app into several helper methods and add better tests (#29)

--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -1,18 +1,24 @@
 # frozen_string_literal: true
 
-require_relative '../app/util'
-
 RSpec.describe Util do
-  describe '.split_message' do
+  describe '::get_answer' do
+    it 'gets an answer from the LLM', :vcr do
+      chat, answer = Util.get_answer('What time does MainStage Brewing in Lyons open on Saturday?')
+      expect(chat).to be_a(RubyLLM::Chat)
+      expect(answer).to match('opens at 11:00 AM on Saturday')
+    end
+  end
+
+  describe '::split_into_messages' do
     it 'returns the original message when under 160 chars' do
       message = 'Short message'
-      result = Util.split_message(message)
+      result = Util.split_into_messages(message)
       expect(result).to eq([message])
     end
 
     it 'splits long messages into multiple chunks' do
       long_message = 'a ' * 100
-      result = Util.split_message(long_message)
+      result = Util.split_into_messages(long_message)
       expect(result).to match([
         start_with('1/2: a'),
         start_with('2/2: a'),
@@ -23,7 +29,7 @@ RSpec.describe Util do
       message = 'this message is 300 long ' * 12
       expect(message.length).to eq(300)
 
-      result = Util.split_message(message)
+      result = Util.split_into_messages(message)
       expect(result).to match([
         start_with('1/2: this message is 300 long'),
         start_with('2/2: message is 300 long this'),
